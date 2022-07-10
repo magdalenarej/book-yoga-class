b2de80ed866a56c330ac2ecc714344cad61c2eeb
magdalenarej <magdalena.szydlik@gmail.com>
2022-07-10 17:44:47 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import "antd/dist/antd.css";
-import BookClassCalendar from "./components/BookClassCalendar";
 import { Layout } from "antd";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BookClassCalendar from "./components/BookClassCalendar";
 import SideMenu from "./components/SideMenu";
+import LoginRegisterCard from "./components/LoginRegisterCard";
 
 const { Content, Sider } = Layout;
 
 const App = () => {
   return (
-    <Layout>
-      <Sider>
-        <SideMenu />
-      </Sider>
-      <Content>
-        <BookClassCalendar />
-      </Content>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Sider>
+          <SideMenu />
+        </Sider>
+        <Content>
+          <Routes>
+            <Route path="/" element={<LoginRegisterCard />}></Route>
+            <Route path="book-class" element={<BookClassCalendar />}></Route>
+          </Routes>
+        </Content>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
