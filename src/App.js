fbeb2ea943412b1eba9cf44a6d41ecc62f0b5c6d
magdalenarej <magdalena.szydlik@gmail.com>
2022-07-14 18:41:42 +0200

    added sider

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import "antd/dist/antd.css";
+import { useState } from "react";
 import { Layout } from "antd";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookClassCalendar from "./components/BookClassCalendar";
 import MainHeader from "./components/MainHeader";
 import LoginRegisterCard from "./components/LoginRegisterCard";
 import AddClass from "./components/AddClass";
+import SideMenu from "./components/SideMenu";
+import UsersClasses from "./components/UsersClasses";
 
-const { Header, Content } = Layout;
+const { Header, Content, Sider } = Layout;
 
 const App = () => {
+  const [collapsed, setCollapsed] = useState(true);
   return (
     <BrowserRouter>
       <Layout style={{ height: "100vh" }}>
         <Header style={{ height: 100 }}>
           <MainHeader />
         </Header>
-        <Content>
-          <Routes>
-            <Route path="/" element={<LoginRegisterCard />}></Route>
-            <Route path="book-class" element={<BookClassCalendar />}></Route>
-            <Route path="add-class" element={<AddClass />}></Route>
-          </Routes>
-        </Content>
+        <Layout>
+          <SideMenu />
+          <Content>
+            <Routes>
+              <Route path="/" element={<LoginRegisterCard />}></Route>
+              <Route path="book-class" element={<BookClassCalendar />}></Route>
+              <Route path="add-class" element={<AddClass />}></Route>
+              <Route path="/my-classes" element={<UsersClasses />}></Route>
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
     </BrowserRouter>
   );
